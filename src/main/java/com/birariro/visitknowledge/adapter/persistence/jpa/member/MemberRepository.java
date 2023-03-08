42b94126388f67b28c59d8dd0cfb83e5dce346ab
JHLee <vps32@naver.com>
2023-03-08 14:41:17 +0900

    feat: email 관련 제거

--- src/main/java/com/birariro/visitknowledge/adapter/persistence/jpa/member/MemberRepository.java
+++ src/main/java/com/birariro/visitknowledge/adapter/persistence/jpa/member/MemberRepository.java
@@ -5,13 +5,10 @@ import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
-import java.util.Optional;
 import java.util.UUID;
 
 @Repository
 public interface MemberRepository extends JpaRepository<Member, UUID> {
-
-    Optional<Member> findByEmail(Email email);
 
     @Query("select m from Member m where m.entityState = 'ACTIVE'")
     List<Member> findActiveByAll();
