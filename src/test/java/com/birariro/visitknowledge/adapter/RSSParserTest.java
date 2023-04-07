133a4310d4a5c54329a36205b8568c91a765abc5
JHLee <vps32@naver.com>
2023-04-07 11:01:36 +0900

    feat: 현구막, jetbrains 사이트 추가

--- src/test/java/com/birariro/visitknowledge/adapter/RSSParserTest.java
+++ src/test/java/com/birariro/visitknowledge/adapter/RSSParserTest.java
@@ -23,24 +23,6 @@ import java.util.stream.Collectors;
 
 public class RSSParserTest {
 
-    @Test
-    public void test() throws IOException, FeedException {
-        //
-        String _url = "https://saramin.github.io/feed.xml";
-        URL url = new URL(_url);
-
-        SyndFeedInput syndFeedInput = new SyndFeedInput();
-        XmlReader xmlReader = new XmlReader(url);
-        SyndFeed build = syndFeedInput.build(xmlReader);
-
-        String title = build.getTitle();
-        System.out.println("title = " + title);
-        build.getEntries().stream().forEach(item -> {
-            System.out.println("item.getTitle() = " + item.getTitle());
-            System.out.println("item.getLink() = " + item.getLink());
-            System.out.println("item.getLink() = " + item.getUri());
-        });
-    }
 
     @Test
     public void test2()  {
@@ -48,7 +30,7 @@ public class RSSParserTest {
 
         try{
             WebClient webClient = WebClient.builder()
-                    .baseUrl("https://hudi.blog/rss.xml")
+                    .baseUrl("https://hyeon9mak.github.io/feed.xml")
                     .codecs(configurer -> configurer.defaultCodecs().maxInMemorySize(10 * 1024 * 1024))
                     .build();
             String block = webClient.get()
