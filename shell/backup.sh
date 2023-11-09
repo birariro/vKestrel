54d3513810a760730359b29745ec7b6c4e6c333d
birariro <vps32@naver.com>
2023-11-09 19:12:07 +0900

    🧠 refactor: shell update

--- shell/backup.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-DB_CONTAINER_NAME=vk-db
-DB_NAME=vk-db
-DB_ID=root
-DB_PWD=0000
-FILE_PATH=.
-
-docker exec ${DB_CONTAINER_NAME} mysqldump -u ${DB_ID} -p${DB_PWD} ${DB_NAME} > ${FILE_PATH}/$(date +%Y-%m-%d-%H.%M.%S).sql
